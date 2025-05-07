e819fd0f07431b5a30151bb2002f9fbbcc68867d
Jia Harisinghani <jia2.harisinghani@gmail.com>
2025-05-07 12:01:33 +0530

    Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.html",  // Include all HTML files in the root directory
+    "./*.html", 
     "./src/**/*.{js,ts,jsx,tsx,html}"
   ],
   theme: {
