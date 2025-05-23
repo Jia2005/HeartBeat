95da8884610645c035391b3ef6c4a879dfc613a4
Jia Harisinghani <jia2.harisinghani@gmail.com>
2025-05-24 00:06:21 +0530

    Added navigations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Question from "./Pages/Question";
+import Happy from "./Pages/Happy";
+import Sad from "./Pages/Sad";
 
 const App = () => {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Question />} />
+        <Route path="/happy" element={<Happy />} />
+        <Route path="/sad" element={<Sad />} />
       </Routes>
     </HashRouter>
   );
